542e1f6ddca7f7c23eac343c8b1d9bd348f15c49
Robinson Gallego <robinson.leal7@gmail.com>
2016-03-29 17:44:10 -0400

    this trasn smh

---
+++ trash/spammer.py
@@ -0,0 +1,13 @@
+#If you'll guess the right number, you'll get prizes!1111111111
+
+num = int(input("Enter a number, 1-10: "))
+if num == ["4","3","2","5","6","7","8","9"]:
+	print("you tried")
+	print("achievement get: be an idiot, BUT tried TOO hard that you aren't gonna get spammed")
+while num == 1:
+	print("ayylmao you got the right number now you can keep this open forever if u want")
+	print("achievement get: be a idiot")
+
+
+else num == ["11"]:
+	print("Gandhi did 9/11.")
