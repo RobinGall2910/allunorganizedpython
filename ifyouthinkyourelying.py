d3b3065a170387edf36cb5863157acb317ebe791
Robinson Gallego <robinson.leal7@gmail.com>
2016-04-05 23:59:25 -0400

    oh-

---
+++ ifyouthinkyourelying.py
@@ -0,0 +1,6 @@
+print("Hey Robinson, do you think I got interesting messages before?"
+    if input == "yes":
+    print("Well, you didn\'t.")
+
+    elif input == "no":
+    print("Of course you did idiot! Better remember that!")
