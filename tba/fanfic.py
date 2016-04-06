09603268ea116910cc93322ed5f0eec8bbbe1631
Robinson Gallego <robinson.leal7@gmail.com>
2016-04-05 20:28:14 -0400

    added more crap to it quickly as possible

--- tba/fanfic.py
+++ tba/fanfic.py
@@ -1,14 +1,17 @@
 # Fan Fictions from other faggots, coded into one Python doc. 2016 Robinson Gallego
 print("Options: Tell me more")
 if int(input("Tell me more")
-# Should say what the hell this is 
-if input == "Tell me more":
+
+elif input == "Tell me more": # Should say what the hell this is
     print("Obviously being done right now ffs")
     print("Your response should be basically")
     print("Oh ok")
     print("Kadie told me to insert that don't hurt me pls");
-#this is some oh ok crash shit, ill eventually fyx it
+#this is some oh ok crash shit, ill eventually fix it
 elif input == "Oh ok":
-while 69 == 69:
-    print("OH OK")
-main()
+    while 69 == 69:
+        print("OH OK")
+elif input == "something":
+    print("you've came to the right story to show you.")
+    print("now, to get started... uh, let me type it out....")
+    wait(5)
